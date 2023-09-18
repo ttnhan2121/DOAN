e2e3d9ca57b156b3a923e205f1b974ed50781296
ttnhan2121 <144778001+ttnhan2121@users.noreply.github.com>
2023-09-19 01:43:49 +0700

    pause

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-const buttonbar = document.querySelector(".show__icon");
-buttonbar.onclick = function(){
-    console.log("click");
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import LinearProgress from '@mui/material/LinearProgress';
+
+export default function LinearIndeterminate() {
+  return (
+    <Box sx={{ width: '100%' }}>
+      <LinearProgress />
+    </Box>
+  );
 }
